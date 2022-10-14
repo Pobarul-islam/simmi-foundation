16ac6645343ebd169e25945f934aea665b8777cb
Pobarul-islam <pobarul3@gmail.com>
2022-10-14 12:33:59 +0600

    slide banner added

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,11 +1,70 @@
-import React from 'react';
+import React from "react";
+import { Carousel } from "react-carousel-minimal";
 
 const Banner = () => {
-    return (
-        <div>
-            
+  const data = [
+    {
+      image: "https://simmifoundation.org/home/admin/upload/img71e24d0f53.jpg",
+    },
+    {
+      image: "https://simmifoundation.org/home/admin/upload/img6e825bdb65.jpeg",
+    },
+    {
+      image: "https://simmifoundation.org/home/admin/upload/imga75b483924.jpg",
+    },
+    {
+      image: "https://simmifoundation.org/home/admin/upload/img2973827efc.jpg",
+    },
+    {
+      image: "https://simmifoundation.org/home/admin/upload/img6e825bdb65.jpeg",
+    },
+    {
+      image: "https://simmifoundation.org/home/admin/upload/img71e24d0f53.jpg",
+    },
+    {
+      image: "https://simmifoundation.org/home/admin/upload/img6e825bdb65.jpeg",
+    },
+    {
+      image: "https://simmifoundation.org/home/admin/upload/imga75b483924.jpg",
+    },
+    {
+      image: "https://simmifoundation.org/home/admin/upload/img2973827efc.jpg",
+    },
+  ];
+
+ 
+  const slideNumberStyle = {
+    fontSize: "20px",
+    fontWeight: "bold",
+  };
+  return (
+    <div className="w-full">
+      <div style={{ textAlign: "center" }}>
+        <div
+          style={{
+            padding: "0 20px",
+          }}
+        >
+          <Carousel
+            data={data}
+            time={2000}
+            width="1400px"
+            height="564px"
+            radius="5px"
+            slideNumberStyle={slideNumberStyle}
+            captionPosition="bottom"
+            automatic={true}
+            dots={true}
+            pauseIconColor="white"
+            pauseIconSize="40px"
+            slideBackgroundColor="darkgrey"
+            slideImageFit="cover"
+            style={{paddingBottom: "10px"}}
+          />
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
