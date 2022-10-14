8bfac223bf759a7b1446085f3f7bbc18de2a5836
Pobarul-islam <pobarul3@gmail.com>
2022-10-14 19:17:19 +0600

    help component added

--- src/Pages/Home/UpEvents.js
+++ src/Pages/Home/UpEvents.js
@@ -1,18 +1,187 @@
 import React from 'react';
- 
 const UpEvents = () => {
     return (
       <div>
-        <div className="card card-compact w-96 bg-base-100 shadow-xl">
-          <figure>
-            <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">Marathon Event</h2>
-        
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary">Buy Now</button>
+        <h2 className='text-5xl text-orange-500'>Current and Upcoming events</h2>
+        <div className="grid mt-20 lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 ml-40 gap-12">
+          <div className="card card-compact w-96 bg-base-100 shadow-xl">
+            <figure>
+              <img
+                src="https://simmifoundation.org/admin/images/events/bg_3.jpg"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">Marathon Event</h2>
+              <i class="fa-solid fa-map-location"></i>
+              <p>
+                event-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-desc
+              </p>
+              <div className="card-actions justify-end">
+                <button className="text-orange-600 text-xl">Read More</button>
+              </div>
+            </div>
+          </div>
+          <div className="card card-compact w-96 bg-base-100 shadow-xl">
+            <figure>
+              <img
+                src="https://simmifoundation.org/admin/images/events/bg_3.jpg"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">Marathon Event</h2>
+              <i class="fa-solid fa-map-location"></i>
+              <p>
+                event-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-desc
+              </p>
+              <div className="card-actions justify-end">
+                <button className="text-orange-600 text-xl">Read More</button>
+              </div>
+            </div>
+          </div>
+          <div className="card card-compact w-96 bg-base-100 shadow-xl">
+            <figure>
+              <img
+                src="https://simmifoundation.org/admin/images/events/bg_3.jpg"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">Marathon Event</h2>
+              <i class="fa-solid fa-map-location"></i>
+              <p>
+                event-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-desc
+              </p>
+              <div className="card-actions justify-end">
+                <button className="text-orange-600 text-xl">Read More</button>
+              </div>
+            </div>
+          </div>
+          <div className="card card-compact w-96 bg-base-100 shadow-xl">
+            <figure>
+              <img
+                src="https://simmifoundation.org/admin/images/events/bg_3.jpg"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">Marathon Event</h2>
+              <i class="fa-solid fa-map-location"></i>
+              <p>
+                event-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-desc
+              </p>
+              <div className="card-actions justify-end">
+                <button className="text-orange-600 text-xl">Read More</button>
+              </div>
+            </div>
+          </div>
+          <div className="card card-compact w-96 bg-base-100 shadow-xl">
+            <figure>
+              <img
+                src="https://simmifoundation.org/admin/images/events/bg_3.jpg"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">Marathon Event</h2>
+              <i class="fa-solid fa-map-location"></i>
+              <p>
+                event-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-desc
+              </p>
+              <div className="card-actions justify-end">
+                <button className="text-orange-600 text-xl">Read More</button>
+              </div>
+            </div>
+          </div>
+          <div className="card card-compact w-96 bg-base-100 shadow-xl">
+            <figure>
+              <img
+                src="https://simmifoundation.org/admin/images/events/bg_3.jpg"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">Marathon Event</h2>
+              <i class="fa-solid fa-map-location"></i>
+              <p>
+                event-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-desc
+              </p>
+              <div className="card-actions justify-end">
+                <button className="text-orange-600 text-xl">Read More</button>
+              </div>
+            </div>
+          </div>
+          <div className="card card-compact w-96 bg-base-100 shadow-xl">
+            <figure>
+              <img
+                src="https://simmifoundation.org/admin/images/events/bg_3.jpg"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">Marathon Event</h2>
+              <i class="fa-solid fa-map-location"></i>
+              <p>
+                event-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-desc
+              </p>
+              <div className="card-actions justify-end">
+                <button className="text-orange-600 text-xl">Read More</button>
+              </div>
+            </div>
+          </div>
+          <div className="card card-compact w-96 bg-base-100 shadow-xl">
+            <figure>
+              <img
+                src="https://simmifoundation.org/admin/images/events/bg_3.jpg"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">Marathon Event</h2>
+              <i class="fa-solid fa-map-location"></i>
+              <p>
+                event-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-desc
+              </p>
+              <div className="card-actions justify-end">
+                <button className="text-orange-600 text-xl">Read More</button>
+              </div>
+            </div>
+          </div>
+          <div className="card card-compact w-96 bg-base-100 shadow-xl">
+            <figure>
+              <img
+                src="https://simmifoundation.org/admin/images/events/bg_3.jpg"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">Marathon Event</h2>
+              <i class="fa-solid fa-map-location"></i>
+              <p>
+                event-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-desc
+              </p>
+              <div className="card-actions justify-end">
+                <button className="text-orange-600 text-xl">Read More</button>
+              </div>
+            </div>
+          </div>
+          <div className="card card-compact w-96 bg-base-100 shadow-xl">
+            <figure>
+              <img
+                src="https://simmifoundation.org/admin/images/events/bg_3.jpg"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">Marathon Event</h2>
+              <i class="fa-solid fa-map-location"></i>
+              <p>
+                event-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-descevent-desc
+              </p>
+              <div className="card-actions justify-end">
+                <button className="text-orange-600 text-xl">Read More</button>
+              </div>
             </div>
           </div>
         </div>
